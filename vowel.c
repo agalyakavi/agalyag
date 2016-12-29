93b72684b6c067c8a15e8232499b828cbad027ca
agalyakavi <agalyakavi97@gmail.com>
2016-12-29 14:07:14 +0530

    Add files via upload

---
+++ vowel.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+#include<conio.h>
+void main()
+{
+char ch;
+printf("enter the character\n");
+scanf("%c",&ch);
+if((ch=='a'||ch=='e'||ch=='i'||ch=='o'||ch=='u'))
+    printf("\nvowel");
+    else
+        printf("\nconsonant");
+    }
+
