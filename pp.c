42e70354ee4e8435e43214d8799a531629c7dbbf
agalyakavi <agalyakavi97@gmail.com>
2017-03-21 16:48:47 +0530

    Add files via upload

---
+++ pp.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+#include<math.h>
+int main()
+{
+int a,i,c;
+printf("Enter the number");
+scanf("%d",&a);
+printf("enter power");
+scanf("%d",&i);
+c=pow(a,i);
+printf("%d",c);
+}
